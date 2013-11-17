1066a1e60a9da75fdbb688f46ab7196b6c47fc4b
Michel Petit <petit.michel@gmail.com>
2013-11-17 10:32:29 +0100

    Starting squares generator code.

--- src/Malenki/Aleavatar.php
+++ src/Malenki/Aleavatar.php
@@ -29,14 +29,18 @@ if(!extension_loaded('gd'))
     throw new \RuntimeException('GD is not available! Aleavatar uses GD to work!');
 }
 
-date_default_timezone_set('UTC'); //put that into CLI script
 
 /**
  * Generate random avatar(s) to populate your websites!
  */
 class Aleavatar
 {
+    const TYPE_SQUARES = 'squares';
+    const TYPE_DISCS = 'discs';
+    const TYPE_RANDOM_SHAPES = 'randomShapes';
+
     protected $arr_colors = array();
+    protected $int_amount_colors = 0;
     protected $size = null;
     protected $image = null;
 
@@ -45,13 +49,85 @@ class Aleavatar
         $this->size = new \stdClass();
         $this->size->width = $width;
         $this->size->height = $height;
-        $this->image = imagecreatetruecolor($width, $height);
+
+        $this->image = imagecreatetruecolor(12, 12);
+
+        $this->createColors();
     }
 
 
 
-    private function createColors()
+    protected function diagonalSquares($int_way)
     {
+        //TODO
+        printf("Diagonal squares selected way %d.\n", $int_way);
+    }
+
+
+
+    protected function squaresInsideSquares($int_amount)
+    {
+        //TODO
+        printf("Squares inside squares selected way with %d squares to generate.\n", $int_amount);
+    }
+
+
+
+    protected function areaSquares()
+    {
+        //TODO
+        printf("Area squares selected.\n", $int_way);
+    }
+
+
+
+    protected function squares()
+    {
+        // Diagonals (1) or squares inside squares (2) or area (3)?
+        $int_choice = rand(1,3);
+
+        // Diagonal squares choices…
+        if($int_choice == 1)
+        {
+            // 1 or 2 diagonals?
+            $int_amount = rand(1, 2);
+
+            // If only one, what way to choose?
+            if($int_amount == 1)
+            {
+                $int_way = rand(1, 2);
+                $this->diagonalSquares($int_way);
+            }
+            else
+            {
+                $this->diagonalSquares(1);
+                $this->diagonalSquares(2);
+            }
+        }
+        elseif($int_choice == 2)
+        {
+            $int_amount = rand(2, 5);
+            $this->squaresInsideSquares($int_amount);
+        }
+        else
+        {
+            $this->areaSquares();
+        }
+    }
+
+
+
+    protected function discs()
+    {
+        //TODO
+    }
+
+
+
+    protected function createColors()
+    {
+        date_default_timezone_set('UTC'); //TODO: put that into CLI script
+
         $str_32 = md5(date('YmdHis') . rand() . sqrt(rand(0, 9)));
         $str_32_rev = strrev($str_32);
         $str_32_rand1 = str_shuffle($str_32);
@@ -73,19 +149,41 @@ class Aleavatar
             );
             $this->arr_colors = array_unique(array_merge($this->arr_colors, $arr_sub_1, $arr_sub_2, $arr_sub_3, $arr_sub_4));
         }
+
+        $this->int_amount_colors = count($this->arr_colors);
     }
 
 
 
-    public function setColors(array $colors)
+    public function amountOfColors($int_amount = 16)
     {
-        $this->arr_colors = $colors;
+        if($int_amount <= count($this->arr_colors))
+        {
+            $this->int_amount_colors = $int_amount;
+        }
+
+        return $this;
     }
 
 
 
-    public function create()
+    public function create($str_type = self::TYPE_SQUARES)
     {
+        if(
+            !in_array(
+                $str_type,
+                array(
+                    self::TYPE_SQUARES,
+                    self::TYPE_DISCS,
+                    self::TYPE_RANDOM_SHAPES
+                )
+            )
+        )
+        {
+            throw new \InvalidArgumentException('Invalid type of generator given!');
+        }
+
+        $this->$str_type();
     }
     
     
