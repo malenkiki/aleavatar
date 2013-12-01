9f82fc499ac2d0c395514c31dcccc90212f4fae6
malenkiki <petit.michel@gmail.com>
2013-12-01 20:49:50 +0100

    Fixed type error

--- src/Malenki/Aleavatar/Primitive/Rectangle.php
+++ src/Malenki/Aleavatar/Primitive/Rectangle.php
@@ -96,9 +96,18 @@ class Rectangle
      */
     public function point($int_x, $int_y)
     {
+        if(is_double($int_x))
+        {
+            $int_x = (integer) $int_x;
+        }
+        
+        if(is_double($int_y))
+        {
+            $int_y = (integer) $int_y;
+        }
         if(!is_integer($int_x) || !is_integer($int_y) || $int_x < 0 || $int_y < 0)
         {
-            throw new \InvalidArgumentException('Corrdinates must be valid positive integers!');
+            throw new \InvalidArgumentException('Coordinates must be valid positive integers!');
         }
 
         $this->point->x = $int_x;
